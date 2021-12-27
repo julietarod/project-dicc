10d2ec51e237d280f4629eb91cf9cd00bb868fd5
julietarod <julietarodriguezba@gmail.com>
2021-12-27 17:04:38 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import images from "./images.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={images} className="App-logo img-fluid" alt="logo" />
+        Baccio
+      </header>
+    </div>
+  );
+}
+
+export default App;
