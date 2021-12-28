1b5d78ecda339f521b38aa6cd5d5d94bcd3c9ba6
julietarod <julietarodriguezba@gmail.com>
2021-12-28 16:32:53 +0100

    created dictionary component + search form + useState

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import images from "./images.jpg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={images} className="App-logo img-fluid" alt="logo" />
-        <a href="#" className="btn btn-primary">
-          {" "}
-          look for it
-        </a>
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={images} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <Dictionary />
+        <footer className="App-footer">
+          <small>
+            Coded by{" "}
+            <a href="https://github.com/julietarod/project-dicc">JulietaRod</a>
+          </small>
+        </footer>
+      </div>
     </div>
   );
 }
